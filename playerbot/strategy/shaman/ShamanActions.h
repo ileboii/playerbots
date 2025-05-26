ded21da6a8fef62d5a86270fe5545016db050104
ile <59745756+ileboii@users.noreply.github.com>
2025-05-26 11:07:45 +0300

    Update ShamanActions.h

--- playerbot/strategy/shaman/ShamanActions.h
+++ playerbot/strategy/shaman/ShamanActions.h
@@ -178,7 +178,7 @@ namespace ai
 
         virtual bool isUseful() override
         {
-            return (!bot->GetGroup() || ai->HasStrategy("wrath of air totem", BotState::BOT_STATE_COMBAT)) && CastTotemAction::isUseful();
+            return (!bot->GetGroup() || ai->HasStrategy("totem air wrath", BotState::BOT_STATE_COMBAT)) && CastTotemAction::isUseful();
         }
 	};
 
@@ -238,7 +238,7 @@ namespace ai
 
         virtual bool isUseful() override
         {
-            return (bot->GetGroup() || ai->HasStrategy("windfury totem", BotState::BOT_STATE_COMBAT)) && CastTotemAction::isUseful();
+            return (bot->GetGroup() || ai->HasStrategy("totem air windfury", BotState::BOT_STATE_COMBAT)) && CastTotemAction::isUseful();
         }
     };
 
@@ -255,7 +255,7 @@ namespace ai
 
         virtual bool isUseful() override
         {
-            return (!bot->GetGroup() || ai->HasStrategy("grace of air totem", BotState::BOT_STATE_COMBAT)) && CastTotemAction::isUseful();
+            return (!bot->GetGroup() || ai->HasStrategy("totem air grace", BotState::BOT_STATE_COMBAT)) && CastTotemAction::isUseful();
         }
     };
 
